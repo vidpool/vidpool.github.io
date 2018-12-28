d2e8ed422f0422da073cb8de26308d310e5df598
Norikazu Kato <norikazu.kato@gunosy.com>
2018-12-28 15:08:01 +0900

    update test page

--- test/js/common/common.js
+++ test/js/common/common.js
@@ -104,3 +104,41 @@ $(function(){
     },300);
   });
 });
+
+
+  //SP動画再生
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+ 
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: 'XXXXXXXXXXXX',
+    playerVars: {
+      playsinline: 1 // 1はtrue、0はfalse
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.mute();
+  event.target.playVideo();
+}
+ 
+function onPlayerStateChange(event) {
+ if (event.data == YT.PlayerState.ENDED) {
+    event.target.playVideo();
+  }
+}
+ 
+
+ 
+
